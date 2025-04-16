9ec95f42f27cc142b65660815c8f82ccbbc7c3c7
細谷　大智 <146915322+hosotani-daichi@users.noreply.github.com>
2025-04-16 14:40:06 +0900

    ウィンドウ表示

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,7 +1,35 @@
+#include "KamataEngine.h"
 #include <Windows.h>
+
+using namespace KamataEngine;
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	
+	//エンジンの初期化
+	KamataEngine::Initialize(L"LE3C_22_ホソタニ_ダイチ");
+
+	//DirectXCommonインスタンスの取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+	//メインループ
+	while (true) {
+	//エンジンの更新
+		if (KamataEngine::Update()) {
+			break;
+		}
+		//描画開始
+		dxCommon->PreDraw();
+
+
+
+
+		//描画終了
+		dxCommon->PostDraw();
+	}
+
+	//エンジンの終了処理
+	KamataEngine::Finalize();
+
 	return 0;
 }
