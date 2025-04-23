93aa6efebe181fd73e8ba90e307a55b39a1878ee
細谷　大智 <146915322+hosotani-daichi@users.noreply.github.com>
2025-04-23 10:56:08 +0900

    あとEffect制作のみ

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -11,7 +11,7 @@ GameScene::~GameScene() {
 void GameScene::Initialize() {
 
 	// 3Dモデルデータの生成
-	modelEffect_ = Model::CreateSphere(4, 4);
+	modelEffect_ = Model::CreateSphere(4, 4);////////SphereをEffectに変えるためにEffectを制作する
 	// カメラの初期化
 	camera_.Initialize();
 	// 菱形の生成
