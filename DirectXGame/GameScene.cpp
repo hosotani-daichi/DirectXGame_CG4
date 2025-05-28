aeaa4eb5ab0ccd48a62b12cc363eeda49f0e9a00
細谷　大智 <146915322+hosotani-daichi@users.noreply.github.com>
2025-05-28 20:05:00 +0900

    菱形描画完成

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -11,7 +11,7 @@ GameScene::~GameScene() {
 void GameScene::Initialize() {
 
 	// 3Dモデルデータの生成
-	modelEffect_ = Model::CreateFromOBJ("Effect"); 
+	modelEffect_ = Model::CreateFromOBJ("Plane"); 
 	// カメラの初期化
 	camera_.Initialize();
 	// 菱形の生成
