b5367a5bf9863b5c01ef982f79cae52a2268a9fa
細谷　大智 <146915322+hosotani-daichi@users.noreply.github.com>
2025-05-28 21:39:01 +0900

    方向と角度変更完成

--- DirectXGame/Effect.cpp
+++ DirectXGame/Effect.cpp
@@ -1,4 +1,9 @@
 #include "Effect.h"
+#include <random>
+std::random_device seed_Generator;
+std::mt19937 RandomEngine(seed_Generator());
+std::uniform_real_distribution<float> RandomSize(0.0f, 1.0f);
+std::uniform_real_distribution<float> RandomRotation(-1.0f, 1.0f);
 
 void Effect::Initialize(Model* model) {
 
@@ -8,12 +13,14 @@ void Effect::Initialize(Model* model) {
 	model_ = model;
 	// ワールド変換の初期化
 	worldTransform_.Initialize();
+	worldTransform_.scale_.y = RandomSize(RandomEngine) * 10.0f;
+	worldTransform_.rotation_.z = RandomRotation(RandomEngine) * 20.0f;
 }
 
 void Effect::Update() {
 
 	// 行列を定数バッファに転送
-	worldTransform_.TransferMatrix();
+	worldTransform_.UpdateMatrix();
 }
 
 void Effect::Draw(Camera& camera) {
