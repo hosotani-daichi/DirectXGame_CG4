0a8d664ea2466291dd3952b675afb6aaeb114923
細谷　大智 <146915322+hosotani-daichi@users.noreply.github.com>
2025-06-19 12:59:06 +0900

    エラー

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -3,17 +3,15 @@
 #include <Windows.h>
 
 using namespace KamataEngine;
-
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
-
 	// エンジンの初期化
-	KamataEngine::Initialize(L"LE3C_22_ホソタニ_ダイチ");
+	Initialize(L"LE3C_22_ホソタニ_ダイチ");
 
 	// DirectXCommonインスタンスの取得
 	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
 
-	// ゲームシーンのインスタンス生成
+	// ゲームシーンの生成
 	GameScene* gameScene = new GameScene();
 	// ゲームシーンの初期化
 	gameScene->Initialize();
@@ -21,7 +19,7 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	// メインループ
 	while (true) {
 		// エンジンの更新
-		if (KamataEngine::Update()) {
+		if (Update()) {
 			break;
 		}
 
@@ -31,6 +29,8 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		// 描画開始
 		dxCommon->PreDraw();
 
+		// ここに描画処理を記述する
+
 		// ゲームシーンの描画
 		gameScene->Draw();
 
@@ -38,13 +38,12 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		dxCommon->PostDraw();
 	}
 
-	// ゲームシーンの解放
 	delete gameScene;
-	// nullptrの代入
+	// ゲームシーンの解放
 	gameScene = nullptr;
 
 	// エンジンの終了処理
-	KamataEngine::Finalize();
+	Finalize();
 
 	return 0;
 }
