fd52bf00c80fbc0dd795eb3b4d60b13928892c46
細谷　大智 <146915322+hosotani-daichi@users.noreply.github.com>
2025-07-14 15:34:22 +0900

    モデル描画完成

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -30,7 +30,7 @@ void GameScene::Initialize() {
 	Model2::StaticInitialize();
 
 	// モデルの生成（正方形）
-	model_ = Model2::CreateSquare();
+	model_ = Model2::Create();
 
 	// テクスチャの読み込み
 	model2Handle_ = TextureManager::Load("uvChecker.png");
--- DirectXGame/Model2/Model2.h
+++ DirectXGame/Model2/Model2.h
@@ -138,12 +138,6 @@ public: // 静的メンバ関数
 	static Model2* CreateSphere(uint32_t divisionVertial = 10, uint32_t divisionHorizontal = 10);
 
 	/// <summary>
-	/// 四角形モデル生成
-	/// </summary>
-	/// <returns></returns>
-	static Model2* CreateSquare();
-
-	/// <summary>
 	/// 描画前処理
 	/// </summary>
 	/// <param name="commandList">描画コマンドリスト</param>
@@ -172,7 +166,7 @@ public: // メンバ関数
 	/// <param name="camera">カメラ</param>
 	/// <param name="textureHadle">テクスチャハンドル</param>
 	/// <param name="objectColor">オブジェクトカラー</param>
-	void Draw(const WorldTransform& worldTransform, const Camera& camera, uint32_t textureHandle, const ObjectColor* objectColor = nullptr);
+	void Draw(const WorldTransform& worldTransform, const Camera& camera, uint32_t textureHadle, const ObjectColor* objectColor = nullptr);
 
 	/// <summary>
 	/// メッシュコンテナを取得
