6d27c278f3d517fb5504fc7c544e6bb3e30de890
細谷　大智 <146915322+hosotani-daichi@users.noreply.github.com>
2025-07-15 16:50:02 +0900

    まだまだ

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -30,7 +30,7 @@ void GameScene::Initialize() {
 	Model2::StaticInitialize();
 
 	// モデルの生成（正方形）
-	model_ = Model2::CreateSquare(5);
+	model_ = Model2::CreateSquare();
 
 	// テクスチャの読み込み
 	model2Handle_ = TextureManager::Load("uvChecker.png");
